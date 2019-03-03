e320a2e310bc41874c14945160ba4d786f8c3574
yuriko <yuri715lily_momo@yahoo.co.jp>
2019-03-03 12:42:30 +0900

    Chapter 5

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,21 @@
 class Task < ApplicationRecord
+	before_validation :set_nameless_name
+	validates :name, presence: true, length: { maximum: 30}
+	validate :validate_name_not_including_comma
+
+	belongs_to :user
+
+
+	scope :recent, -> {order(created_at: :desc)}
+
+
+	private
+
+	def validate_name_not_including_comma
+		errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
+	end
+
+	def set_nameless_name
+		self.name = '名前なし' if name.blank?
+	end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+	has_secure_password
+
+	validates :name, presence: true
+	validates :email, presence: true
+
+	has_many :tasks
+end
