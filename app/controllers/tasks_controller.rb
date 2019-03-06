1c4eb46ebe6e6ac4a1b0cefd69d885b461314230
yuriko <yuri715lily_momo@yahoo.co.jp>
2019-03-06 19:05:22 +0900

    Ransac_search_function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,8 @@ class TasksController < ApplicationController
 
 
   def index
-  	@tasks = current_user.tasks.order(created_at: :desc)
+    @q = current_user.tasks.ransack(params[:q])
+    @tasks = @q.result(distinct: true).recent
   end
 
   def confirm_new
