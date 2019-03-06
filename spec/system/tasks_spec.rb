9121503712ebb85ba68cbaac5718cf4fcc912f41
yuriko <yuri715lily_momo@yahoo.co.jp>
2019-03-06 16:03:16 +0900

    add_spec_for_詳細

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,9 +2,9 @@
 require 'rails_helper'
 
 describe 'タスク管理機能' ,type: :system do
-	describe '一覧表示機能' do
 		let(:user_g) { FactoryBot.create(:user, name: 'ユーザーG', email: 'g@example.com')}
 		let(:user_h) { FactoryBot.create(:user, name: 'ユーザーH', email: 'h@example.com')}
+		let(:task_a) { FactoryBot.create(:task, name: '最初のタスク',user: user_g)}
 
 		before do
 			FactoryBot.create(:task, name: '最初のタスク', user: user_g)
@@ -14,7 +14,8 @@ describe 'タスク管理機能' ,type: :system do
 			click_button 'ログインする'
 		end
 
-		context 'ユーザーGがログインしている時' do
+		describe '一覧表示機能' do
+		 context 'ユーザーGがログインしている時' do
 			let(:login_user) { user_g }
 			it 'ユーザーGが作成したタスクが表示される' do
 				expect(page).to have_content '最初のタスク'
@@ -25,6 +26,21 @@ describe 'タスク管理機能' ,type: :system do
 			let(:login_user) { user_h }
 			it 'ユーザDが作成したタスクが表示されない' do
 				expect(page).to have_no_content '最初のタスク'
+			end
+		end
+	end
+
+
+	describe '詳細表示機能' do
+		context 'ユーザーGがログインしてる時' do
+			let(:login_user) { user_g }
+
+			before do
+				visit task_path(task_a)
+			end
+
+			it 'ユーザーGが作成したタスクが表示される' do
+				expect(page).to have_content '最初のタスク'
 			end
 		end
 	end
